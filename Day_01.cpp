421437c92b16cabcc4d2b2062bf2e88661dc1922
Sravya Potnuru <86221880+SravyaPotnuru@users.noreply.github.com>
2022-09-30 20:42:36 +0530

    Create Day_01.cpp

---
+++ Day_01.cpp
@@ -0,0 +1,35 @@
+//Remove Nth Node from the end of the LinkedList
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+      int len=0;
+        ListNode*temp=head;
+        while(temp!=NULL){
+            temp=temp->next;
+            len++;
+        }
+        int pos=len-n+1;
+        
+        if(pos==1){
+            ListNode*temp=head;
+            head=head->next;
+            temp->next=NULL;
+            delete temp;
+        }
+        else{
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+            int cnt=1;
+            while(cnt<pos){
+                prev=curr;
+                curr=curr->next;
+                cnt++;
+            }
+            prev->next=curr->next;
+            curr->next=NULL;
+            delete curr;
+        }
+        return head;
+    }
+};
