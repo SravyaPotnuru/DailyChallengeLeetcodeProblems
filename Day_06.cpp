a09101969350fce6a73057d54428aadc54660873
Sravya Potnuru <86221880+SravyaPotnuru@users.noreply.github.com>
2022-10-08 23:53:57 +0530

    Create Day_06.cpp

---
+++ Day_06.cpp
@@ -0,0 +1,17 @@
+//Minimum Time to make Rope colourful
+
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+      if(colors.length()==0||colors.length()==1) return 0;
+      int sum=0;
+      for(int i=1;i<colors.length();i++){
+          if(colors[i]==colors[i-1]){
+              sum+=min(neededTime[i],neededTime[i-1]);
+              neededTime[i]=max(neededTime[i],neededTime[i-1]);
+          }
+      }
+      return sum;
+    }
+};
+
