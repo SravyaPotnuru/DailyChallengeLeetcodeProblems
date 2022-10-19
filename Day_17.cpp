934d48f086cfb4df55fba2a960474d531156edd8
Sravya Potnuru <86221880+SravyaPotnuru@users.noreply.github.com>
2022-10-19 21:00:15 +0530

    Create Day_17.cpp

---
+++ Day_17.cpp
@@ -0,0 +1,44 @@
+//Delete the Middle Node of a Linked List
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getMid(ListNode* head){     
+        int pos=0;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            pos++;
+        }
+        return pos;
+    }
+    ListNode* deleteMiddle(ListNode* head) {
+     if(head==NULL || head->next==NULL){   // Handling the case of a single node or a null node
+            return NULL;
+        }
+        int position=getMid(head);
+        
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        while(curr!=NULL && position>0){
+            prev=curr;
+            curr=curr->next;
+            position--;
+        }
+        prev->next=curr->next;
+        
+        return head; 
+    }
+    
+};
