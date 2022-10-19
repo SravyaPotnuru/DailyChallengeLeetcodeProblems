6cb1c235b5301d2a0684942f9b1f756efe252862
Sravya Potnuru <86221880+SravyaPotnuru@users.noreply.github.com>
2022-10-19 21:08:36 +0530

    Create Day_20.cpp

---
+++ Day_20.cpp
@@ -0,0 +1,14 @@
+//Check if the Sentence is pangram
+
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        set<char>s;
+        for(int i=0;i<sentence.size();i++){
+            s.insert(sentence[i]);
+        }
+        if(s.size()==26) return true;
+        else return false;
+    }
+};
+
