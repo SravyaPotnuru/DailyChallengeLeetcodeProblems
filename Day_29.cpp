f6f5f88275de3beb30744af9319d5d7c6800215c
Sravya Potnuru <86221880+SravyaPotnuru@users.noreply.github.com>
2022-11-03 22:15:18 +0530

    Create Day_29.cpp

---
+++ Day_29.cpp
@@ -0,0 +1,16 @@
+//Continuous Subarray Sum
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+       int sum=0;
+       unordered_map<int,int>m;
+       for(int i=0;i<nums.size();i++){
+           sum+=nums[i];
+           if(sum%k==0 && i>0) return true;
+           if(m.count(sum%k) &&  i-m[sum%k]>=2) return true;
+           if(m.count(sum%k)==0) m[sum%k]=i;
+       }
+       return false;
+    }
+};
