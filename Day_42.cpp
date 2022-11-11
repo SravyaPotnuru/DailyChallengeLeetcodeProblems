5a8799abe303cd67ba56ccf0f828d01943886fb4
Sravya Potnuru <86221880+SravyaPotnuru@users.noreply.github.com>
2022-11-11 14:43:53 +0530

    Create Day_42.cpp

---
+++ Day_42.cpp
@@ -0,0 +1,18 @@
+//make the string Great
+
+class Solution {
+public:
+    string makeGood(string s) {
+      if(s.length()==0)
+      return "";
+       string ans;
+       for(int i=0;i<s.size();i++){
+           ans.push_back(s[i]);
+           while(ans.size() && (ans.back()==s[i+1]+32 || ans.back()==s[i+1]-32)){
+               ans.pop_back();
+               i++;
+           }
+       } 
+       return ans;
+    }
+};
